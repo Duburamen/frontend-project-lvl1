35fe0ac48835c8bc3e0112011af0102d90b549cb
Vadim <squintjester@gmail.com>
2022-06-20 18:30:48 +0600

    upd bin/

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from 'readline-sync';
-console.log ('Welcome to the Brain Games!');
+import { userName } from '../src/cli.js';
 
-const name = readlineSync.question('May I have your name? ');
-console.log ('Hello, ' + name);
+console.log ('Welcome to the Brain Games!');
+userName();

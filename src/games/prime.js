b30da46d434f05de461071a4cf15e9dad1c96e9a
Vadim <squintjester@gmail.com>
2022-07-06 00:56:02 +0600

    edited prime() function

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,25 +1,27 @@
 import readlineSync from 'readline-sync';
 import { greetings, congrats } from '../index.js';
 
+const primeFunction = (a) => {
+  let isPrime = true;
+  if (a === 1) {
+    return false;
+  }
+  if (a > 1) {
+    for (let i = 2; i < a / 2; i += 1) {
+      if (a % i === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+  }
+  return isPrime;
+};
+
 const prime = () => {
   const name = greetings();
   for (let counter = 1; counter <= 3; counter += 1) {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
-    const primeFunction = (a) => {
-      let isPrime = true;
-      if (a === 1) {
-        return false;
-      }
-      if (a > 1) {
-        for (let i = 2; i < a / 2; i += 1) {
-          if (a % i === 0) {
-            isPrime = false;
-            break;
-          }
-        }
-      }
-      return isPrime;
-    };
+
     console.log(`Question: ${randomNumber}`);
     const answer = readlineSync.question('Your answer: ');
     if (((answer === 'yes') && (primeFunction(randomNumber) === true)) || ((answer === 'no') && (primeFunction(randomNumber) === false))) {

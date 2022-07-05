fda76fbf056304419c9309e0372cc2cab8f57f81
Vadim <squintjester@gmail.com>
2022-07-06 00:50:25 +0600

    create brain-prime.js

---
+++ bin/brain-prime.js
@@ -0,0 +1,6 @@
+#!/usr/bin/env node
+// import readlineSync from 'readline-sync';
+import prime from '../src/games/prime.js';
+
+console.log('Welcome to the Brain Games!');
+prime();
---
+++ src/games/prime.js
@@ -0,0 +1,36 @@
+import readlineSync from 'readline-sync';
+import { greetings, congrats } from '../index.js';
+
+const prime = () => {
+  const name = greetings();
+  for (let counter = 1; counter <= 3; counter += 1) {
+    const randomNumber = Math.floor(Math.random() * 100) + 1;
+    const primeFunction = (a) => {
+      let isPrime = true;
+      if (a === 1) {
+        return false;
+      }
+      if (a > 1) {
+        for (let i = 2; i < a / 2; i += 1) {
+          if (a % i === 0) {
+            isPrime = false;
+            break;
+          }
+        }
+      }
+      return isPrime;
+    };
+    console.log(`Question: ${randomNumber}`);
+    const answer = readlineSync.question('Your answer: ');
+    if (((answer === 'yes') && (primeFunction(randomNumber) === true)) || ((answer === 'no') && (primeFunction(randomNumber) === false))) {
+      console.log('Correct!');
+    } else {
+      const correctAnswer = (primeFunction(randomNumber) === true) ? 'yes' : 'no';
+      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
+      return;
+    }
+  }
+  congrats(name);
+};
+
+export default prime;

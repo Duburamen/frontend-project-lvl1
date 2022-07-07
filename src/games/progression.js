8d1091b80793c3b44a17763ddfbff8cc9d931415
Vadim <squintjester@gmail.com>
2022-07-07 20:10:33 +0600

    reduce progression.js complexity

--- src/games/progression.js
+++ src/games/progression.js
@@ -1,22 +1,23 @@
 import readlineSync from 'readline-sync';
 import { greetings, congrats } from '../index.js';
 
+const progressionGenerator = (a, b) => {
+  const array = [];
+  const count = 10;
+  for (let i = 0; array.length < count; i += 1) {
+    const sum = a + b * i;
+    array.push(sum);
+  }
+  return array;
+};
+
 const progression = () => {
   const name = greetings();
-  const count = 10;
   for (let counter = 1; counter <= 3; counter += 1) {
     const randomNumber = Math.floor(Math.random() * 50);
     const step = Math.floor(Math.random() * 10) + 1;
     const randomArrayIndex = Math.floor(Math.random() * 9);
-    const progressionFunction = (a, b) => {
-      const array = [];
-      for (let i = 0; array.length < count; i += 1) {
-        const sum = a + b * i;
-        array.push(sum);
-      }
-      return array;
-    };
-    const arrayOutput = progressionFunction(randomNumber, step);
+    const arrayOutput = progressionGenerator(randomNumber, step);
     const correctAnswer = arrayOutput[randomArrayIndex];
     arrayOutput[randomArrayIndex] = '..';
     console.log(`Question: ${arrayOutput.join(' ')}`);

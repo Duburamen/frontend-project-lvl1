c1e89df264070f6ad8109222c991ec0706157c9f
Vadim <squintjester@gmail.com>
2022-07-07 22:20:43 +0600

    create and import summaryYesNo function

--- src/games/even.js
+++ src/games/even.js
@@ -1,22 +1,17 @@
-import readlineSync from 'readline-sync';
+import { greetings, summaryYesNo, congrats } from '../index.js';
 
 const even = () => {
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}`);
+  const name = greetings();
   console.log('Answer "yes" if the number is even, otherwise answer "no"');
   for (let counter = 1; counter <= 3; counter += 1) {
     const randomNumber = Math.floor(Math.random() * 100);
-    console.log(`Question: ${randomNumber}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (((answer === 'yes') && (randomNumber % 2 === 0)) || ((answer === 'no') && (randomNumber % 2 === 1))) {
-      console.log('Correct!');
-    } else {
-      const correctAnswer = (randomNumber % 2 === 0) ? 'yes' : 'no';
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
-      console.log(`Let's try again, ${name}!`);
+    const correctAnswer = (randomNumber % 2 === 0) ? 'yes' : 'no';
+    const question = `Question: ${randomNumber}`;
+
+    if (summaryYesNo(question, correctAnswer, name) === false) {
       return;
     }
   }
-  console.log(`Congratulations, ${name}!`);
+  congrats(name);
 };
 export default even;

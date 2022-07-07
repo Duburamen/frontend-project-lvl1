eaeca354b47cc649db47c30c7769d4a677795d17
Vadim <squintjester@gmail.com>
2022-07-07 22:23:17 +0600

    create summary, summaryYesNo functions

--- src/index.js
+++ src/index.js
@@ -6,6 +6,30 @@ export const greetings = () => {
   return name;
 };
 
+export const summary = (question, correctAnswer, userName) => {
+  console.log(question);
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (userAnswer === correctAnswer.toString()) {
+    console.log('Correct!');
+  } else {
+    console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}."\nLet's try again, ${userName}`);
+    return false;
+  }
+  return true;
+};
+
+export const summaryYesNo = (question, correctAnswer, userName) => {
+  console.log(question);
+  const userAnswer = readlineSync.question('Your answer: ');
+  if (((userAnswer === 'yes') && (correctAnswer === 'yes')) || ((userAnswer === 'no') && (correctAnswer === 'no'))) {
+    console.log('Correct!');
+  } else {
+    console.log(`"${userAnswer}" is wrong answer ;(. Correct answer was "${correctAnswer}."\nLet's try again, ${userName}`);
+    return false;
+  }
+  return true;
+};
+
 export const congrats = (name) => {
   console.log(`Congratulations, ${name}!`);
 };

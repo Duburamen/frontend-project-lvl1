759b0f2418a873fe54fc22317d51d8cfe90d8345
Vadim <squintjester@gmail.com>
2022-07-07 22:21:34 +0600

    implement summary function

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -1,25 +1,21 @@
-import readlineSync from 'readline-sync';
-import { greetings, congrats } from '../index.js';
+import { greetings, summary, congrats } from '../index.js';
+
+const gcdGenerator = (a, b) => {
+  if (!b) {
+    return a;
+  }
+  return gcdGenerator(b, a % b);
+};
 
 const gcd = () => {
   const name = greetings();
   for (let counter = 1; counter <= 3; counter += 1) {
     const randomNumberFirst = Math.floor(Math.random() * 50);
     const randomNumberSecond = Math.floor(Math.random() * 50);
-    const gcdFunction = (a, b) => {
-      if (!b) {
-        return a;
-      }
-      return gcdFunction(b, a % b);
-    };
-    const correctAnswer = gcdFunction(randomNumberFirst, randomNumberSecond);
-    console.log(`Question: ${randomNumberFirst} ${randomNumberSecond}`);
-    const answer = readlineSync.question('Your answer: ');
-    if (answer === correctAnswer.toString()) {
-      console.log('Correct!');
-    } else {
-      console.log(`"${answer}" is wrong answer ;(. Correct answer was "${correctAnswer}"`);
-      console.log(`Let's try again, ${name}!`);
+    const correctAnswer = gcdGenerator(randomNumberFirst, randomNumberSecond);
+    const question = `Question: ${randomNumberFirst} ${randomNumberSecond}`;
+
+    if (summary(question, correctAnswer, name) === false) {
       return;
     }
   }

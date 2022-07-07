ae2d0b87f83fd166cde4cc77dc0f0d44d7f19484
Vadim <squintjester@gmail.com>
2022-07-07 22:30:11 +0600

    simplify isPrime() function

--- src/games/prime.js
+++ src/games/prime.js
@@ -1,14 +1,12 @@
 import { greetings, summaryYesNo, congrats } from '../index.js';
 
 const isPrime = (a) => {
-  if (a === 1) {
+  if (a <= 1) {
     return false;
   }
-  if (a > 1) {
-    for (let i = 2; i <= a / 2; i += 1) {
-      if (a % i === 0) {
-        return false;
-      }
+  for (let i = 2; i <= a / 2; i += 1) {
+    if (a % i === 0) {
+      return false;
     }
   }
   return true;
@@ -18,7 +16,7 @@ const prime = () => {
   const name = greetings();
   for (let counter = 1; counter <= 3; counter += 1) {
     const randomNumber = Math.floor(Math.random() * 100) + 1;
-    const correctAnswer = (isPrime(randomNumber) === true) ? 'yes' : 'no';
+    const correctAnswer = isPrime(randomNumber) === true ? 'yes' : 'no';
     const question = `Question: ${randomNumber}`;
 
     if (summaryYesNo(question, correctAnswer, name) === false) {
